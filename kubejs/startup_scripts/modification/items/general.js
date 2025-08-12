8504c197cd5431c81bd4ef2a99c113c0a4e22e27
kohara <kohy.creates@gmail.com>
2025-08-12 17:20:11 +0200

    Remove Dungeons Weaponry mod

--- kubejs/startup_scripts/modification/items/general.js
+++
@@ -1,5 +0,0 @@
-ItemEvents.modification(event => {
-	event.modify('structure_gel:building_tool', item => {
-		item.setFireResistant(true);
-	})
-})
--- kubejs/startup_scripts/registry/attributes.js
+++
@@ -1,5 +0,0 @@
-const $RangedAttribute = Java.loadClass("net.minecraft.world.entity.ai.attributes.RangedAttribute")
-StartupEvents.registry('attribute', event => {
-	event.createCustom('kubejs:harvest_level', () => new $RangedAttribute("Harvest Level", 0, 0, 10).setSyncable(true))
-	event.createCustom('kubejs:mining_speed', () => new $RangedAttribute("Base Mining Speed", 0, 0, 10).setSyncable(true))
-})
