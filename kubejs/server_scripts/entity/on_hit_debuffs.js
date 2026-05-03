b0d3cfeeaa0883913fe7b74cc5edee611d133b6c
kohara <kohy.creates@gmail.com>
2026-05-03 15:17:38 +0200

    ✨ Rough setup for on-hit debuffs from mobs.

--- kubejs/server_scripts/entity/on_hit_debuffs.js
+++ kubejs/server_scripts/entity/on_hit_debuffs.js
@@ -1,37 +1,82 @@
-// const OnHitDebuffConfig = [
-//     {
-//         mobs: [
-//             /witherstormmod:sickened_/,
-//             'wither_skeleton'
-//         ],
-//         effect: {
-//             id: 'wither',
-//             chance: 1,
-//             level: 1
-//         }
-//     },
-//     {
-//         mobs: [
-//             'spider'
-//         ],
-//         effect: {
-//             id: 'poison',
-//             chance: [0, null, null, 0.15, null, 0.25],
-//             level: 1
-//         }
-//     }
-// ]
+const OnHitDebuffConfig = [
+	{
+		mobs: [
+			'spider'
+		],
+		id: 'poison',
+		duration: [0, null, null, 4, null, 6],
+		chance: [0, null, null, 15, null, 25]
+	},
+	{
+		mobs: [
+			'phantom',
+			'witherstormmod:sickened_phantom'
+		],
+		id: 'weakness',
+		duration: [0, 8, null, 12, null, 16],
+		chance: [0, 75, null, null, null, 100]
+	},
+	{
+		mobs: [
+			'slime',
+			'twilightforest:maze_slime',
+			'twilightforest:slime_beetle'
+		],
+		id: 'slowness',
+		duration: [3, 4, null, 7, null, 9],
+		chance: [25, null, null, 35, null, 40]
+	},
+	{
+		mobs: [
+			/born_in_chaos_v1:skeleton_thrasher/
+		],
+		id: 'kubejs:armor_shred',
+		duration: 28,
+		chance: 25
+	},
+	{
+		mobs: [
+			'zombie',
+			'husk',
+			'witherstormmod:sickened_zombie'
+		],
+		id: 'majruszsdifficulty:bleeding',
+		chance: [25, 30, 35, 40, 45, 50],
+		duration: [9, 12, 15, 18, 21, 24],
+		level: [1, null, 2, 3, null, 4]
+	}
+];
 
-// EntityEvents.hurt('player', event => {
-//     const player = event.getPlayer();
-//     if (event.getEntity() === player) {
-//         const attacker = event.getSource().getActual();
-//         for (let entry of OnHitDebuffConfig) {
-//             for (let mob of entry.mobs) {
-//                 if (mob instanceof RegExp) {
-//                     if (RegExp.)
-//                 }
-//             }
-//         }
-//     }
-// });
+EntityEvents.hurt('player', event => {
+	const player = event.getPlayer();
+	const attacker = event.getSource().getActual();
+	const type = attacker.type;
+
+	let chosenEntry;
+
+	entryIterator: for (let entry of OnHitDebuffConfig) {
+		typeChecker: for (let mob of entry.mobs) {
+			if (mob instanceof RegExp && mob.test(type)) {
+				chosenEntry = entry;
+				break entryIterator;
+			}
+			else if ((!mob.includes(':') && mob == type.replace('minecraft:', '')) || mob == type) {
+				chosenEntry = entry;
+				break entryIterator;
+			}
+			else continue typeChecker;
+		}
+	}
+
+	if (chosenEntry) {
+		let currentStage = global.getCurrentChapter(server);
+		let chance = chosenEntry.chance, amplifier = chosenEntry.level || 1, duration = chosenEntry.duration;
+		if (Array.isArray(chance)) chance = EntityModifications._logic.getStageValue(chance, currentStage);
+		if (Array.isArray(amplifier)) amplifier = EntityModifications._logic.getStageValue(amplifier, currentStage);
+		if (Array.isArray(duration)) duration = EntityModifications._logic.getStageValue(duration, currentStage);
+
+		if (global.isRandomChance(chance)) {
+			player.addEffect(new $MobEffectInstance(chosenEntry.id, Math.ceil(duration * 20), amplifier - 1));
+		}
+	}
+});
