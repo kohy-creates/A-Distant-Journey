4fcb995611b8af235d2039b7e8d77f8a3f39626c
kohara <kohy.creates@gmail.com>
2026-05-28 21:44:32 +0200

    ✨ Fix all items acting like bandages (thanks KubeJS for being jank as fuck)

--- kubejs/server_scripts/item/bandage.js
+++ kubejs/server_scripts/item/bandage.js
@@ -1,16 +1,18 @@
-ItemEvents.rightClicked(/kubejs:.*bandage/, event => {
+ItemEvents.rightClicked(event => {
 	const item = event.getItem();
-	const player = event.getPlayer();
-	const raycastTarget = event.getTarget();
-	let healTarget;
-	if (raycastTarget && raycastTarget.entity) {
-		healTarget = raycastTarget.entity;
+	if (item.id == 'kubejs:bandage' || item.id == 'kubejs:golden_bandage') {
+		const player = event.getPlayer();
+		const raycastTarget = event.getTarget();
+		let healTarget;
+		if (raycastTarget && raycastTarget.entity) {
+			healTarget = raycastTarget.entity;
+		}
+		else {
+			healTarget = player;
+		}
+		if (healTarget.getHealth() == healTarget.getMaxHealth()) return;
+		healTarget.heal(item.id == 'kubejs:golden_bandage' ? 5.0 : 2.001);
+		player.addItemCooldown(item.getItem(), 5);
+		item.shrink(1);
 	}
-	else {
-		healTarget = player;
-	}
-	if (healTarget.getHealth() == healTarget.getMaxHealth()) return;
-	healTarget.heal(item.id == 'kubejs:golden_bandage' ? 5.0 : 2.001);
-	player.addItemCooldown(item.getItem(), 4);
-	item.shrink(1);
 });
